ccd5b28bca0af65981641c253e4af5d4bd13846a
mdunten <mdunten@BOS-MAC-2285.local>
2015-02-11 10:00:51 -0600

    Updates

--- krbUserBute.rb
+++ krbUserBute.rb
@@ -15,33 +15,22 @@ class Metasploit4 < Msf::Auxiliary
 
   def initialize(info = {})
     super(update_info(info,
-      'Name' => 'MS14-068 Microsfot Kerberos Checksum Validation Vulnerability',
+      'Name' => 'Kerberos Username Brute forcer',
       'Description' => %q{
-        This module exploits a vulnerability in the Microsoft Kerberos implementation. The problem
-        exists in the verification of the Privilege Attribute Certificate (PAC) from a Kerberos TGS
-        request, where a domain user may forge a PAC with arbitrary privileges, including
-        Domain Administrator. This module requests a TGT ticket with a forged PAC and exports it to
-        a MIT Kerberos Credential Cache file. It can be loaded on Windows systems with the Mimikatz
-        help. It has been tested successfully on Windows 2008.
+        This module brute forces valid AD usernames by inspecting the Kerberos preauth errors.
       },
       'Author' =>
         [
-          'Tom Maddock', # Vulnerability discovery
-          'Sylvain Monne', # pykek framework and exploit
-          'juan vazquez' # Metasploit module
+          'juan vazquez', # Kerberos Client
+          'michael dunten' #module
         ],
       'References' =>
         [
-          ['CVE', '2014-6324'],
-          ['MSB', 'MS14-068'],
-          ['OSVDB', '114751'],
-          ['URL', 'http://blogs.technet.com/b/srd/archive/2014/11/18/additional-information-about-cve-2014-6324.aspx'],
-          ['URL', 'https://labs.mwrinfosecurity.com/blog/2014/12/16/digging-into-ms14-068-exploitation-and-defence/'],
-          ['URL', 'https://github.com/bidord/pykek'],
-          ['URL', 'https://community.rapid7.com/community/metasploit/blog/2014/12/25/12-days-of-haxmas-ms14-068-now-in-metasploit']
+          ['CVE', 'N/A'],
+          ['MSB', 'N/a'],
         ],
       'License' => MSF_LICENSE,
-      'DisclosureDate' => 'Nov 18 2014'
+      'DisclosureDate' => 'Feb 10 2014'
     ))
 
     register_options(
@@ -52,10 +41,11 @@ class Metasploit4 < Msf::Auxiliary
             true, 'The file that contains a list of probable users accounts.',
             File.join(Msf::Config.install_root, 'data', 'wordlists', 'unix_users.txt')
           ]),
+        OptString.new('RHOST', [ true, 'IP Address of Domain Controller' ]),
         OptString.new('PASSWORD', [ true, 'The Domain User password' ]),
-        OptString.new('DOMAIN', [ true, 'The Domain (upper case) Ex: DEMO.LOCAL' ]),
-        #OptString.new('USER_SID', [ true, 'The Domain User SID, Ex: S-1-5-21-1755879683-3641577184-3486455962-1000'])
+        OptString.new('DOMAIN', [ true, 'The Domain (upper case) Ex: DEMO.LOCAL' ])
       ], self.class)
+      deregister_options('RHOSTS')
   end
 
   def run
